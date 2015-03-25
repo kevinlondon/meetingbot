a72fbd06073f719cb242043b63ce20a1ff5cfa2c
kevinlondon <kevinlondon@gmail.com>
2015-03-24 22:30:18 -0700

    Better parsing of events. Put those into their own class.

--- room_indicator.py
+++ room_indicator.py
@@ -2,7 +2,7 @@ import sys
 import json
 import httplib2
 import arrow
-from operator import itemgetter
+from operator import attrgetter
 
 from oauth2client.client import SignedJwtAssertionCredentials
 from apiclient.discovery import build
@@ -13,9 +13,40 @@ USER = "kevin@wiredrive.com"
 
 class Calendar(object):
 
-    def __init__(self, raw_data):
-        self.pk = raw_data['id']
-        self.summary = raw_data['summary']
+    ATTRIBUTES = ['id', 'summary']
+
+    def __init__(self, data):
+        self._data = data
+        self._assign_attributes()
+
+    def _assign_attributes(self):
+        self.id = self._data['id']
+        self.summary = self._data['summary']
+
+
+class Event(object):
+
+    ATTRIBUTES = ['id', 'status', 'summary', 'attendees', 'description']
+    DATETIMES = ['start', 'end']
+
+    def __init__(self, data):
+        self._data = data
+        self._assign_attributes()
+
+    def __repr__(self):
+        return "{0} - {1}\t{2}".format(self.start, self.end, self.summary)
+
+    def _assign_attributes(self):
+        self._assign_datetimes()
+        for attribute in self.ATTRIBUTES:
+            value = self._data.get(attribute)
+            setattr(self, attribute, value)
+
+    def _assign_datetimes(self):
+        for datetime_attr in self.DATETIMES:
+            value = self._data[datetime_attr]['dateTime']
+            setattr(self, datetime_attr, value)
+
 
 
 def authenticate_client():
@@ -35,28 +66,31 @@ def get_useful_calendars(calendar_list):
     useful_calendars = []
     for entry in calendar_list['items']:
         calendar = Calendar(entry)
-        if "Room" in calendar.summary:
+        if "Conference Room" in calendar.summary:
             useful_calendars.append(calendar)
 
     return useful_calendars
 
 
 def list_events(calendar, calendar_service):
-    now = arrow.utcnow()
-    tomorrow = arrow.utcnow().replace(days=+1)
+    now = arrow.now()
+    tomorrow = arrow.now().replace(days=+1)
 
     events = calendar_service.events().list(
-        calendarId=calendar.pk,
+        calendarId=calendar.id,
         timeMin=str(now),
         timeMax=str(tomorrow),
         showDeleted=False,
+        singleEvents=True,
     ).execute()
 
     print "\n\nRoom: ", calendar.summary
-    event_list = [event for event in events['items'] if 'start' in event]
-    sorted_events = sorted(event_list, key=itemgetter('start'))
+    event_list = [Event(data) for data in events['items'] if 'start' in data]
+
+    sorted_events = sorted(event_list, key=attrgetter('start'))
     for event in sorted_events:
-        print event['start'], event['summary']
+        print event
+        #print event['start'], event['summary']
 
 
 def main():
